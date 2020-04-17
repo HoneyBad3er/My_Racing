a370aa536129f87eac20553ae932ca3ec52effbb
HoneyBad3er <maram1r0v@yandex.ru>
2020-04-17 17:09:51 +0300

    Test changing

---
+++ Racing/test/dtm_create_test.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+#include "concreatecarfactory.h"
+#include "car.h"
+
+
+TEST(DTM_CREATING_TEST, test_1) {
+    std::shared_ptr<Car_Factory> DTM_Factory = std::make_shared<DTM_Car_Factory>();
+    std::shared_ptr<Car> dtm_car = DTM_Factory->Create_Car();
+    EXPECT_EQ(dtm_car->get_power(), 750);
+    EXPECT_EQ(dtm_car->get_weight(), 1200);
+    EXPECT_EQ(dtm_car->get_max_speed(), 380);
+    EXPECT_EQ(dtm_car->get_controllability(), 70);
+}
